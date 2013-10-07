e49c64302969f25314f9c852a3c877e8fb03b18e
Jouke Thiemo Waleson <jouke@waleson.com>
2013-10-08 00:11:37 +0200

    Simplify, simplify, simplify
    As I said earlier, Express makes things easier. In this case, it added a
    send method the the res(ponse) which does what you usually want.
    
    See http://expressjs.com/api.html#res.send for details.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(8080);
